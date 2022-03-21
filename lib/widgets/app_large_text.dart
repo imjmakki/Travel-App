361dd95d32246e8c1c860df38eff7ed994fca619
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-21 11:32:30 +0300

    Travel Project

--- lib/widgets/app_large_text.dart
+++ lib/widgets/app_large_text.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppLargeText extends StatelessWidget {
-  int size;
+  double size;
   final String text;
   final Color color;
   AppLargeText(
