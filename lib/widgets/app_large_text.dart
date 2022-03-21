df80fd33fe3de209425537042ee2c3efd63c3686
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-21 11:25:26 +0300

    Travel Project

---
+++ lib/widgets/app_large_text.dart
@@ -0,0 +1,11 @@
+class AppLargeText extends StatelessWidget {
+  int size;
+  final String text;
+  final Color color;
+  const AppLargeText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
