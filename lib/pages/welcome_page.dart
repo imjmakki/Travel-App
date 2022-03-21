59d34f807c80dd2483f53c169d4628fc4ebe3b55
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-21 10:41:21 +0300

    Travel Project

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,13 @@
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
