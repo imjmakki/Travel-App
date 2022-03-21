14252a8fdb964cc53c752664cad8a95712d4c7c1
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-21 11:32:05 +0300

    Travel Project

--- lib/widgets/app_large_text.dart
+++ lib/widgets/app_large_text.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AppLargeText extends StatelessWidget {
   int size;
   final String text;
   final Color color;
   AppLargeText(
-      {Key? key, this.size = 30, required this.text, required this.color})
+      {Key? key, this.size = 30, required this.text, this.color = Colors.black})
       : super(key: key);
 
   @override
